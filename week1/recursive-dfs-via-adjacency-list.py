f5ad07bfcfeebec6fee6e5ba937e229fe854b7c9
Chau <ChauTNguyen96@gmail.com>
2019-01-09 08:55:23 -0800

    Add recursive dfs via adjacency list

---
+++ week1/recursive-dfs-via-adjacency-list.py
@@ -0,0 +1,42 @@
+graphs = [
+    {
+        'A': ['B', 'D'],
+        'B': ['C'],
+        'C': ['D'],
+        'D': [],
+    },
+    {
+        'A': ['B', 'C', 'E'],
+        'B': ['D', 'F'],
+        'C': ['G'],
+        'D': [],
+        'E': [],
+        'F': ['E'],
+        'G': [],
+    },
+]
+
+def get_neighbors(g, v):
+    return g[v]
+
+def dfs_helper(g, v, visited):
+    print(v)
+    visited.append(v)
+    # `get_neighbors` depends on the graph implementation. In this case,
+    # we're using an adjacency list, and so getting a list of
+    # neighbor vertices is O(1).
+    #
+    # This is important because that's the pro of adjacency lists. However,
+    # they are not good at quickly stating if 2 vertices `x` and `y` are adjacent,
+    # as you have to iterate over the list of neighbor vertices to determine this.
+    for vertex in get_neighbors(g, v):
+        if vertex not in visited:
+            dfs_helper(g, vertex, visited)
+
+def dfs(g, root):
+    dfs_helper(g, root, [])
+
+if __name__ == '__main__':
+    dfs(graphs[0], 'A')
+    print()
+    dfs(graphs[1], 'A')
